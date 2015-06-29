39d342d82cd26be0b8745400c0dec418ccbeabf6
Smart-Core <artem.digi@gmail.com>
2015-06-29 23:35:10 +0600

    add LegacyPaginator

--- Controller/UnicatWidgetController.php
+++ Controller/UnicatWidgetController.php
@@ -2,9 +2,9 @@
 
 namespace SmartCore\Module\Unicat\Controller;
 
-use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Exception\NotValidCurrentPageException;
 use Pagerfanta\Pagerfanta;
+use Smart\CoreBundle\Pagerfanta\SimpleDoctrineORMAdapter;
 use SmartCore\Bundle\CMSBundle\Module\CacheTrait;
 use SmartCore\Bundle\CMSBundle\Module\NodeTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -56,11 +56,19 @@ class UnicatWidgetController extends Controller
         return new Response($categoryTree);
     }
 
+    /**
+     * @param array $criteria
+     * @param array $orderBy
+     * @param int $limit
+     * @param null $offset
+     *
+     * @return \Symfony\Component\HttpFoundation\Response|\Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
     public function getItemsAction(array $criteria, array $orderBy = null, $limit = 10, $offset = null)
     {
         $ucm = $this->get('unicat')->getConfigurationManager($this->configuration_id);
 
-        $pagerfanta = new Pagerfanta(new DoctrineORMAdapter($ucm->getFindItemsQuery($criteria, $orderBy, $limit, $offset)));
+        $pagerfanta = new Pagerfanta(new SimpleDoctrineORMAdapter($ucm->getFindItemsQuery($criteria, $orderBy, $limit, $offset)));
         $pagerfanta->setMaxPerPage($limit);
 
         try {
